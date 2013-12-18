3ac520bf51766fc6188e32b162c79991f31859ee
jspenc72 <jspenc72@gmail.com>
2013-12-18 01:09:09 -0700

    Fix iOS 7 status bar issue in MainViewController.m
    Just replace the viewWillAppear in MainViewController.m with this:
    
    - (void)viewWillAppear:(BOOL)animated
    {
    // View defaults to full size.  If you want to customize the view's
    size, or its subviews (e.g. webView),
    // you can do so here.
    //Lower screen 20px on ios 7
    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7) {
        CGRect viewBounds = [self.webView bounds];
        viewBounds.origin.y = 20;
        viewBounds.size.height = viewBounds.size.height - 20;
        self.webView.frame = viewBounds;
    }
    [super viewWillAppear:animated];
    }

--- app/ubmv3.3.0/ubm/platforms/ios/ubm/Classes/MainViewController.m
+++ app/ubmv3.3.0/ubm/platforms/ios/ubm/Classes/MainViewController.m
@@ -63,11 +63,24 @@
 
 #pragma mark View lifecycle
 
-- (void)viewWillAppear:(BOOL)animated
+/*- (void)viewWillAppear:(BOOL)animated
 {
     // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
     // you can do so here.
 
+    [super viewWillAppear:animated];
+}*/
+- (void)viewWillAppear:(BOOL)animated
+{
+    // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
+    // you can do so here.
+    //Lower screen 20px on ios 7
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7) {
+        CGRect viewBounds = [self.webView bounds];
+        viewBounds.origin.y = 20;
+        viewBounds.size.height = viewBounds.size.height - 20;
+        self.webView.frame = viewBounds;
+    }
     [super viewWillAppear:animated];
 }
 
