493a457116bef4f2b3e2316b69b84dd8855c2608
jspenc72 <jspenc72@gmail.com>
2013-12-18 00:16:14 -0700

    Compiled new UBM App using phonegap 3.3.0 command line tools.
    Jesses-MacBook-Pro:ubmv3.3.0 WREAdmin$ cordova create ubm
    com.universalbusinessmodel.app ubm
    Creating a new cordova project with name "ubm" and id
    "com.universalbusinessmodel.app" at location
    "/Users/WREAdmin/ubm/app/ubmv3.3.0/ubm"
    Jesses-MacBook-Pro:ubmv3.3.0 WREAdmin$ ls
    ubm
    Jesses-MacBook-Pro:ubmv3.3.0 WREAdmin$ cd ubm/
    Jesses-MacBook-Pro:ubm WREAdmin$ ls
    merges		platforms	plugins		www
    Jesses-MacBook-Pro:ubm WREAdmin$ pwd
    /Users/WREAdmin/ubm/app/ubmv3.3.0/ubm
    Jesses-MacBook-Pro:ubm WREAdmin$ ls
    merges		platforms	plugins		www
    Jesses-MacBook-Pro:ubm WREAdmin$ phonegap build ios
    [phonegap] detecting iOS SDK environment...
    [phonegap] using the local environment
    [phonegap] adding the iOS platform...
    [phonegap] compiling iOS...
    [phonegap] successfully compiled iOS app

--- app/MyNotes/MyNotes/platforms/ios/www/cordova_plugins.js
+++ app/MyNotes/MyNotes/platforms/ios/www/cordova_plugins.js
@@ -424,25 +424,5 @@ module.exports = [
             "window.Entry"
         ]
     }
-];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-    "org.apache.cordova.device": "0.2.5",
-    "org.apache.cordova.network-information": "0.2.5",
-    "org.apache.cordova.battery-status": "0.2.5",
-    "org.apache.cordova.device-motion": "0.2.4",
-    "org.apache.cordova.device-orientation": "0.3.3",
-    "org.apache.cordova.geolocation": "0.3.4",
-    "org.apache.cordova.camera": "0.2.5",
-    "org.apache.cordova.media-capture": "0.2.5",
-    "org.apache.cordova.media": "0.2.6",
-    "org.apache.cordova.file-transfer": "0.4.0",
-    "org.apache.cordova.dialogs": "0.2.4",
-    "org.apache.cordova.vibration": "0.3.5",
-    "org.apache.cordova.contacts": "0.2.6",
-    "org.apache.cordova.console": "0.2.5",
-    "org.apache.cordova.file": "0.2.5"
-}
-// BOTTOM OF METADATA
+]
 });
