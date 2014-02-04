6a5a949eabce6523b19f03bede394eab2b8f3f6b
jspenc72 <jspenc72@gmail.com>
2014-02-04 16:40:10 -0700

    Updated Cordova Phonegap Platform to 3.3.0
    Whats New:
    
    Updated Cordova Phonegap Platform to 3.3.0
    Installed Network information Plugin
    Added the ability to add a resolution to an open items from within the
    app.
    Numerous Bug Fixes.

--- app/ubmv3.3.0/ubm/platforms/ios/www/js/index.js
+++ app/ubmv3.3.0/ubm/platforms/ios/www/js/index.js
@@ -35,15 +35,12 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
+    // Check Network Connection on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+		 var networkState = navigator.connection.type;
+		 if(networkState=="none"){
+		 	$(".offline").css("visibility","visible");
+		 }
     }
 };
