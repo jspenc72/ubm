3c1f678c098882b9596867a9e1fb79d2c84a2cd0
agustaf9 <agustafson15@gmail.com>
2014-03-04 21:20:11 -0700

    fixed signin form

--- app/modelcreator/ubm/www/js/bootstrap-tree.js
+++ app/modelcreator/ubm/www/js/bootstrap-tree.js
@@ -1,16 +1,15 @@
 function coolify() {
-	$('.tree > ul').attr('role', 'tree').find('ul').attr('role', 'group');
-	$('.tree li ul > li').hide();
-	$('.tree').find('li:has(ul)').addClass('parent_li').attr('role', 'treeitem').find(' > span').attr('title', 'Collapse this branch').on('click', function(e) {
-		var children = $(this).parent('li.parent_li').find(' > ul > li');
-		if (children.is(':visible')) {
-			children.hide('fast');
-			$(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
-		} else {
-			children.show('fast');
-			$(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
-		}
-		e.stopPropagation();
-	});
+	$('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Expand this branch');
+    $('.tree li.parent_li > span').on('click', function (e) {
+        var children = $(this).parent('li.parent_li').find(' > ul > li');
+        if (children.is(":visible")) {
+            children.hide('fast');
+            $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+        } else {
+            children.show('fast');
+            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+        }
+        e.stopPropagation();
+    });
 }
 
