41964d0f4c7c5ba701b47d5635814c0e5c0677a1
jspenc72 <jspenc72@gmail.com>
2014-03-05 18:18:48 -0700

    Added Heirarchical View of Model Objects.
    Added Heirarchical View of Model Objects.

--- app/modelcreator/ubm/platforms/ios/www/js/bootstrap-tree.js
+++ app/modelcreator/ubm/platforms/ios/www/js/bootstrap-tree.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
+function coolify() {
 	$('.tree > ul').attr('role', 'tree').find('ul').attr('role', 'group');
-	$('.tree').find('li:has(ul)').addClass('parent_li').attr('role', 'treeitem').find(' > span').attr('title', 'Collapse this branch').on('click', function (e) {
-        var children = $(this).parent('li.parent_li').find(' > ul > li');
-        if (children.is(':visible')) {
-    		children.hide('fast');
-    		$(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
-        }
-        else {
-    		children.show('fast');
-    		$(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
-        }
-        e.stopPropagation();
-    });
-});
+	$('.tree li ul > li').hide();
+	$('.tree').find('li:has(ul)').addClass('parent_li').attr('role', 'treeitem').find(' > span').attr('title', 'Collapse this branch').on('click', function(e) {
+		var children = $(this).parent('li.parent_li').find(' > ul > li');
+		if (children.is(':visible')) {
+			children.hide('fast');
+			$(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+		} else {
+			children.show('fast');
+			$(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+		}
+		e.stopPropagation();
+	});
+}
+
