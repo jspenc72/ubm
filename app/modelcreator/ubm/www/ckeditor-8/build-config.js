f0feaa7242dda074b12468ed77cf3266e9799db6
jspenc72 <jspenc72@gmail.com>
2014-03-27 01:00:58 -0600

    Added UBM Product Creator.
    UBM Product creator uses a wysiwyg editor to allow users to create html
    products with an experience nearly comparable to what they would get in
    Microsoft word or Open Office.
    NOTE:
    The Save function doesn’t currently work and will need to be modified
    to allow cross domain transfer of information and storage of the
    contents of each product in the api.universalbusinessmodel.com database.

---
+++ app/modelcreator/ubm/www/ckeditor-8/build-config.js
@@ -0,0 +1,141 @@
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/66c1baefb665577e7478b7e08d5479be
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/66c1baefb665577e7478b7e08d5479be
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'bootstrapck',
+	preset: 'full',
+	ignore: [
+		'dev',
+		'.gitignore',
+		'.gitattributes',
+		'README.md',
+		'.mailmap'
+	],
+	plugins : {
+		'a11yhelp' : 1,
+		'autogrow' : 1,
+		'autosave' : 1,
+		'backgrounds' : 1,
+		'base64image' : 1,
+		'basicstyles' : 1,
+		'bidi' : 1,
+		'blockquote' : 1,
+		'clipboard' : 1,
+		'codemirror' : 1,
+		'colorbutton' : 1,
+		'colordialog' : 1,
+		'confighelper' : 1,
+		'contextmenu' : 1,
+		'devtools' : 1,
+		'dialogadvtab' : 1,
+		'div' : 1,
+		'elementspath' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'filebrowser' : 1,
+		'find' : 1,
+		'flash' : 1,
+		'floatingspace' : 1,
+		'font' : 1,
+		'format' : 1,
+		'forms' : 1,
+		'googledocs' : 1,
+		'horizontalrule' : 1,
+		'htmlwriter' : 1,
+		'iframe' : 1,
+		'iframedialog' : 1,
+		'image' : 1,
+		'image2' : 1,
+		'imagebrowser' : 1,
+		'imagepaste' : 1,
+		'imageresize' : 1,
+		'indentblock' : 1,
+		'indentlist' : 1,
+		'inlinecancel' : 1,
+		'inlinesave' : 1,
+		'justify' : 1,
+		'language' : 1,
+		'link' : 1,
+		'list' : 1,
+		'liststyle' : 1,
+		'magicline' : 1,
+		'mathjax' : 1,
+		'maximize' : 1,
+		'newpage' : 1,
+		'oembed' : 1,
+		'page2images' : 1,
+		'pagebreak' : 1,
+		'panelbutton' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'pbckcode' : 1,
+		'preview' : 1,
+		'print' : 1,
+		'qrc' : 1,
+		'removeformat' : 1,
+		'removespan' : 1,
+		'resize' : 1,
+		'resizewithwindow' : 1,
+		'save' : 1,
+		'scayt' : 1,
+		'selectall' : 1,
+		'sharedspace' : 1,
+		'showblocks' : 1,
+		'showborders' : 1,
+		'showprotected' : 1,
+		'slideshow' : 1,
+		'smiley' : 1,
+		'sourcearea' : 1,
+		'specialchar' : 1,
+		'spoiler' : 1,
+		'stylescombo' : 1,
+		'stylesheetparser' : 1,
+		'syntaxhighlight' : 1,
+		'tab' : 1,
+		'table' : 1,
+		'tableresize' : 1,
+		'tabletools' : 1,
+		'textselection' : 1,
+		'textsignature' : 1,
+		'texzilla' : 1,
+		'tliyoutube' : 1,
+		'toolbar' : 1,
+		'uicolor' : 1,
+		'undo' : 1,
+		'wenzgmap' : 1,
+		'widget' : 1,
+		'wordcount' : 1,
+		'wsc' : 1,
+		'wysiwygarea' : 1,
+		'xmas' : 1,
+		'xml' : 1,
+		'xmltemplates' : 1,
+		'youtube' : 1
+	},
+	languages : {
+		'en' : 1
+	}
+};
---
+++ app/modelcreator/ubm/www/ckeditor-8/plugins/inlinecancel/plugin.js
@@ -0,0 +1,20 @@
+﻿CKEDITOR.plugins.add( 'inlinecancel',
+{
+	init: function( editor )
+	{
+		editor.addCommand( 'inlinecancel',
+			{
+				exec : function( editor )
+				{
+					if(confirm("Cancel and reload page? (All progress since last save will be lost.)"))
+						location.reload(true);
+				}
+			});
+		editor.ui.addButton( 'Inlinecancel',
+		{
+			label: 'Cancel and Reload Page',
+			command: 'inlinecancel',
+			icon: this.path + 'images/inlinecancel.png'
+		} );
+	}
+} );
