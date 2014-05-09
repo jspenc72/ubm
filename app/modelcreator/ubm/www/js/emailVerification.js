becfb03e998d292e67b762dfea9577eb1e76bf2a
Adam Gustafson <agustafson15@gmail.com>
2014-05-09 16:56:20 -0600

    Formatted
    Formatted js and worked on input verification.

--- app/modelcreator/ubm/www/js/emailVerification.js
+++ app/modelcreator/ubm/www/js/emailVerification.js
@@ -1,15 +1,14 @@
-function checkEmailVerification() {
-function checkEmailVerification() {
-	$.getJSON('http://api.universalbusinessmodel.com/CheckEmailVerification.php?callback=?', {//JSONP Request
-		username : window.username
-	}, function(res, status) {
-		alert(res.email_activation_status);
-		if (res.email_activation_status == "1"){
-			$().toastmessage('showSuccessToast', "Account Successfully Verified");
-			window.location = "#ubmsuite_SelectBusinessModel";
-		} else {
-			window.location = "#account_verification";
-			$().toastmessage('showErrorToast', "Your Account Has Not Been Activated");
-		}
-	});
-}
+    function checkEmailVerification() {
+        $.getJSON('http://api.universalbusinessmodel.com/CheckEmailVerification.php?callback=?', { //JSONP Request
+            username: window.username
+        }, function(res, status) {
+            alert(res.email_activation_status);
+            if (res.email_activation_status == "1") {
+                $().toastmessage('showSuccessToast', "Account Successfully Verified");
+                window.location = "#ubmsuite_SelectBusinessModel";
+            } else {
+                window.location = "#account_verification";
+                $().toastmessage('showErrorToast', "Your Account Has Not Been Activated");
+            }
+        });
+    }
