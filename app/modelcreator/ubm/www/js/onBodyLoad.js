0b971b035e9004eda6233ddce0b4997e8ca9f754
Adam Gustafson <agustafson15@gmail.com>
2014-05-19 16:40:03 -0600

    fixed some errors
    fixed some errors

--- app/modelcreator/ubm/www/js/onBodyLoad.js
+++ app/modelcreator/ubm/www/js/onBodyLoad.js
@@ -172,7 +172,6 @@ function onLoadBody() {
                                                                                         }
                                                                                     }
                                                                                 }
-
                                                                             }
                                                                         }
                                                                     }
