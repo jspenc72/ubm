454c58bc6c0230049d0c9f615ffbfaebc76dca26
jspenc72 <jspenc72@gmail.com>
2014-05-19 14:54:43 -0600

    Fixed open item popup on org chart page

--- app/modelcreator/ubm/www/js/onBodyLoad.js
+++ app/modelcreator/ubm/www/js/onBodyLoad.js
@@ -133,6 +133,14 @@ function onLoadBody() {
 																									//Get the popup on the current page.
 																									link.setAttribute("href", '#ubmsuite_modelDashboard_openItem_popup');
 																									//Set the Attribute of the open item button to the popup on the current page.																									
+																								}else{
+																									if(strarray[1] == "ubmsuite_mcs_my_organizational_chart"){
+																										var link = document.getElementById("openItem_popup_button");
+																										//Get the popup on the current page.
+																										link.setAttribute("href", '#ubmsuite_mcs_my_organizational_chart_openItem_popup');
+																										//Set the Attribute of the open item button to the popup on the current page.
+
+																									}
 																								}
 																							}
 																						}
