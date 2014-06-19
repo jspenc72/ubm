944eb29b56c6f379000ef2a73a20e6e530494dd4
Adam Gustafson <agustafson15@gmail.com>
2014-06-19 15:51:51 -0600

    Modified PHP Scripts
    Modified add objects php scripts. Removed unnecessary comments and
    changed modelId to modelUUID

--- app/modelcreator/ubm/www/php/ubms_modelcreationsuite_model_add_Policy.php
+++ app/modelcreator/ubm/www/php/ubms_modelcreationsuite_model_add_Policy.php
@@ -10,7 +10,7 @@ if ($conn -> connect_error) {
 }
 
 //INSERT
-$v2 = "'" . $conn -> real_escape_string($activeModelId) . "'";
+$v2 = "'" . $conn -> real_escape_string($activeModelUUID) . "'";
 $v3 = "'" . $conn -> real_escape_string($activePositionId) . "'";
 $v4 = "'" . $conn -> real_escape_string($activeJobDescriptionUUID) . "'";
 $v5 = "'" . $conn -> real_escape_string($activePolicyId) . "'";
@@ -19,19 +19,6 @@ $v7 = "'" . $conn -> real_escape_string($activeStepId) . "'";
 $v8 = "'" . $conn -> real_escape_string($activeTaskId) . "'";
 $v9 = "'" . $conn -> real_escape_string($username) . "'";
 
-//SELECT
-//1. Find the current active model UUID based on the id of the specification. THIS will return a list of all instances of a specification.
-// $activeJobDescriptionUUID = 1;
-	// $sqlsel1="SELECT * FROM ubm_modelcreationsuite_heirarchy_object_antiSolipsism_UUID WHERE jobDescription_id=$v4";		//Select all 
-	// $rs1=$conn->query($sqlsel1);
-	// if($rs1 === false) {
-	  // trigger_error('Wrong SQL: ' . $sqlsel1 . ' Error: ' . $conn->error, E_USER_ERROR);
-	// } else{
-		// while ($items1 = $rs1->fetch_assoc()) {
-			// $activeJobDescriptionUUID = stripslashes($items1['UUID']);
-		// }
-	// }
-// INSERT
 //2. Insert the activePolicyId into the UUID table, creating a new instance of the Policy specification.
 $sqlins = "INSERT INTO ubm_modelcreationsuite_heirarchy_object_antiSolipsism_UUID (legal_entity_id, model_id, position_id, jobDescription_id, policy_id, procedure_id, step_id, task_id, created_by) VALUES ('0','0','0','0',$v5,'0','0','0',$v9)";
 if ($conn -> query($sqlins) === false) {
@@ -60,99 +47,3 @@ if ($conn -> query($sqlins) === false) {
 			}		
 	}
 }
-
-/*SELECT
- $sqlsel="SELECT * FROM ubm_mcs_app_resolutions WHERE openitemid=$openitemid";
-
- $rs=$conn->query($sqlsel);
-
- if($rs === false) {
- trigger_error('Wrong SQL: ' . $sqlsel . ' Error: ' . $conn->error, E_USER_ERROR);
- } else {
- $rows_returned = $rs->num_rows;
- }
- //UPDATE
- //$v1="'" . $conn->real_escape_string($disposition) . "'";
- $sql="UPDATE `ubm_mcs_app_openitems` SET number_resolutions=$rows_returned WHERE id=$openitemid";
- if($conn->query($sql) === false) {
- trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
- } else {
- $affected_rows = $conn->affected_rows;
- echo $_GET['callback'] . '(' . "{'message' : 'The number of affected rows is $affected_rows, the number of resolutions is: $rows_returned the openitemid modified was $openitemid!'}" . ')';
- }
- 
-//Send Email Message
-$to = "$inviteEmail";
-$subject = "$username added you to the following model #\"$activeModelId\"!";
-$message = "You have been invited as a $memberRole user to participate in building a business model!";
-$from = "notify@universalbusinessmodel.com";
-$headers = "From:" . $from;
-mail($to, $subject, $message, $headers);
-//echo "Mail Sent.";
-/*http://www.pontikis.net/blog/how-to-use-php-improved-mysqli-extension-and-why-you-should
- * SELECT
- $sql='SELECT col1, col2, col3 FROM table1 WHERE condition';
-
- $rs=$conn->query($sql);
-
- if($rs === false) {
- trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
- } else {
- $rows_returned = $rs->num_rows;
- }
- * Iterate over Recordset
- $rs->data_seek(0);
- while($row = $rs->fetch_assoc()){
- echo $row['col1'] . '<br>';
- }
- * 	Store all values to array
- $rs=$conn->query($sql);
-
- if($rs === false) {
- trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
- } else {
- $arr = $rs->fetch_all(MYSQLI_ASSOC);
- }
- foreach($arr as $row) {
- echo $row['co1'];
- }
- * 		Record count
- $rows_returned = $rs->num_rows;
- * Move inside recordset
- $rs->data_seek(10);
- *	Free memory
-
- $rs->free();
- *
- * INSERT
- $v1="'" . $conn->real_escape_string('col1_value') . "'";
-
- $sql="INSERT INTO tbl (col1_varchar, col2_number) VALUES ($v1,10)";
-
- if($conn->query($sql) === false) {
- trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
- } else {
- $last_inserted_id = $conn->insert_id;
- $affected_rows = $conn->affected_rows;
- }
- * UPDATE
- $v1="'" . $conn->real_escape_string('col1_value') . "'";
-
- $sql="UPDATE tbl SET col1_varchar=$v1, col2_number=1 WHERE id>10";
-
- if($conn->query($sql) === false) {
- trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
- } else {
- $affected_rows = $conn->affected_rows;
- }
- * DELETE
- $sql="DELETE FROM tbl WHERE id>10";
-
- if($conn->query($sql) === false) {
- trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
- } else {
- $affected_rows = $conn->affected_rows;
- }
-
- * */
-?>
