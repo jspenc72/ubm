eece6a0548ee0b8c5b719b64a68fc8ffce5bd90d
jspenc72 <jspenc72@gmail.com>
2014-07-09 20:16:01 -0600

    Updated Org Chart Page
    1. Updated setting of activePositionUUID
    2. Added Position Summary, Position Pay Range Low, Position Pay Range
    High,
    3. Reset Create Position form when new position is created.
    4. Updated Remove Position from My Model to send the position UUID
    5. Added Needed variables to the global get variables.
    6. Updated Create Position PHP controller
    7. Updated Remove Position PHP controller

--- app/modelcreator/ubm/www/php/ubms_modelcreationsuite_model_remove_Positions.php
+++ app/modelcreator/ubm/www/php/ubms_modelcreationsuite_model_remove_Positions.php
@@ -14,25 +14,30 @@ if ($conn->connect_error) {
 
 //INSERT
 $v2 = "'" . $conn->real_escape_string($activeModelUUID) . "'";
-$v3 = "'" . $conn->real_escape_string($activePositionId) . "'";
+$v3 = "'" . $conn->real_escape_string($activePositionUUID) . "'";
 $v4 = "'" . $conn->real_escape_string($activeJobDescriptionId) . "'";
 $v5 = "'" . $conn->real_escape_string($activePolicyId) . "'";
 $v6 = "'" . $conn->real_escape_string($activeProcedureId) . "'";
 $v7 = "'" . $conn->real_escape_string($activeStepId) . "'";
 $v8 = "'" . $conn->real_escape_string($activeTaskId) . "'";
+$v9 = "'" . $conn->real_escape_string($activeAncestorUUID) . "'";
+$v10 = "'" . $conn->real_escape_string($activeDescendantUUID) . "'";
    //1. Delete all links that connect the Position to a model. */
 $sql = "
- DELETE link
- FROM ubm_modelcreationsuite_heirarchy_object_closureTable a, 
-  		ubm_modelcreationsuite_heirarchy_object_closureTable link, 
-  		ubm_modelcreationsuite_heirarchy_object_closureTable d, 
-  		ubm_modelcreationsuite_heirarchy_object_closureTable to_delete
- WHERE a.ancestor_id=link.ancestor_id
-	AND d.descendant_id=link.descendant_id
-	AND a.descendant_id=to_delete.ancestor_id AND d.ancestor_id=to_delete.descendant_id
-	AND (to_delete.ancestor_id=$v3 OR to_delete.descendant_id=$v3)
-	AND to_delete.path_length<2";
+DELETE link
+ FROM ubm_model_position_closure a, 
+      ubm_model_position_closure link, 
+      ubm_model_position_closure d, 
+      ubm_model_position_closure to_delete
+ WHERE a.ancestor_UUID=link.ancestor_UUID
+  AND d.descendant_UUID=link.descendant_UUID
+  AND a.descendant_UUID=to_delete.ancestor_UUID AND d.ancestor_UUID=to_delete.descendant_UUID
+  AND (to_delete.ancestor_UUID=$v9 
+         AND to_delete.descendant_UUID=$v10)
+  AND to_delete.path_length<2";
 
+// Third Attempt at deletion - This will remove everything but the self links.     
+     //Remove Position from Position Closure
 //NOTE $v4 is the UUID of whatever needs to be deleted from the closureTable.
 //THIS WILL NOT REMOVE GRANDPARENT / GRANDCHILD Relationships that skip the UUID given.
 //e.g. (if the parent of a grandchild is deleted, the child is still linked to the grandparent with a relationship of length 1.)
@@ -40,6 +45,6 @@ $sql = "
 if ($conn->query($sql) === false) {
     trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
 } else {
-    $affected_rows = $conn->affected_rows;
-    echo $_GET['callback'] . '(' . "{'message' : 'The number of affected rows is $affected_rows. the Model we attempted to modify was $v2'}" . ')';
+    $affected_rows = $conn->affected_rows;   
+    echo $_GET['callback'] . '(' . "{'message' : 'The number of affected rows is $affected_rows. the Model we attempted to modify was $v9'}" . ')';
 }
